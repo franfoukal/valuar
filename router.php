5881533b35f1aa559c989a3a62de736690be6854
franfoukal <franciscofoukal@gmail.com>
2019-12-27 20:18:18 -0300

    se puede entrar a los productos por query string

--- router.php
+++ router.php
@@ -3,7 +3,7 @@ $request = $_SERVER['REQUEST_URI'];
 $path = parse_url($request, PHP_URL_PATH);
 $segments = explode('/', $path); //Separa la URL donde van los "/" y los guarda en un array. El 0 es "valuar"
 
-// var_dump($request);
+// var_dump($request, $segments[2]);
 
 function isHome($request){
     if($request == '/valuar/' || $request == "" || $request == "/valuar/home"){
@@ -53,6 +53,11 @@ function router($request)
             http_response_code(404);
             require __DIR__ . '/views/404.php';
             break;
+    }
+
+    switch($segments[2]){
+        case 'product':
+
     }
 }
 ?>
--- views/product.php
+++ views/product.php
@@ -2,6 +2,14 @@
     $file = "assets/data-source/products.json";
     $conn = file_get_contents($file);
     $products = json_decode($conn, true);
+    $selectedProduct = "";
+
+    foreach ($products as $p) {
+      if($p["id"] == $_GET["id"]){
+        $selectedProduct = $p;
+      }
+    }
+
 ?>
 <div class="container">
     <div class="card p-0 my-3">
@@ -21,19 +29,19 @@
                 <div class="carousel-inner" role="listbox">
                   <!--First slide-->
                   <div class="carousel-item active">
-                    <img class="d-block img-fluid product-img" src="./assets/img/products/<?=$products[0]["photos"][0]?>"
+                    <img class="d-block img-fluid product-img" src="./assets/img/products/<?=$selectedProduct["photos"][0]?>"
                       alt="First slide">
                   </div>
                  
                   <!--Second slide-->
                   <div class="carousel-item">
-                    <img class="d-block img-fluid product-img" src="./assets/img/products/<?=$products[0]["photos"][1]?>"
+                    <img class="d-block img-fluid product-img" src="./assets/img/products/<?=$selectedProduct["photos"][1]?>"
                       alt="Second slide">
                   </div>
                   
                   <!--Third slide-->
                   <div class="carousel-item">
-                    <img class="d-block w-100" src="./assets/img/products/<?=$products[0]["photos"][2]?>"
+                    <img class="d-block w-100" src="./assets/img/products/<?=$selectedProduct["photos"][2]?>"
                       alt="Third slide">
                   </div>
                   
@@ -56,7 +64,7 @@
           <div class="col-12 col-md-7 my-auto">
             <div class="row">
               <div class="col-12">
-                <h3 class='my-1 item-title card-title'>Anillo <?=$products[0]["name"]?></h3>
+                <h3 class='my-1 item-title card-title'>Anillo <?=$selectedProduct["name"]?></h3>
                 <ol class="breadcrumb font-small p-0">
                   <li class="breadcrumb-item">
                     <a class='' href="#">Joyas</a>
@@ -74,7 +82,7 @@
             </div>
             <div class="row">
               <div class="col-12">
-                <h3 class='h3 amount plain-text my-3 mx-auto'>$ <span id='precio'><?=$products[0]["price"]?></span></h3>
+                <h3 class='h3 amount plain-text my-3 mx-auto'>$ <span id='precio'><?=$selectedProduct["price"]?></span></h3>
               </div>
             </div>
             <div class="row">
